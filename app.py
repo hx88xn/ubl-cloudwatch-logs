3974c5f3e26fa17450b91ab2ee6889cfaf9c0d83
Muhammad Hassan Abbasi <109933658+hx88xn@users.noreply.github.com>
2025-12-18 17:32:58 +0500

    groups logs by uuid

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-# from src.utils.helper import filter_uuid
-# from src.logs import fetch_logs
+from src.utils.helper import filter_uuid
+from src.logs import fetch_logs
 
-# logs = fetch_logs(hours=1, limit=10000, page=1, page_size=50)
-
-# uuid_map = filter_uuid(logs['logs'])
-
-# print(uuid_map)
+logs = fetch_logs(hours=1, limit=10, page=1, page_size=50)
+print(logs)
+uuid_map = filter_uuid(logs)
+print(uuid_map)
--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,18 +1,18 @@
-# from collections import defaultdict
-# import re
+from collections import defaultdict
+import re
 
-# UUID4_REGEX = re.compile(
-#     r"\b[0-9a-fA-F]{8}-"
-#     r"[0-9a-fA-F]{4}-"
-#     r"4[0-9a-fA-F]{3}-"
-#     r"[89abAB][0-9a-fA-F]{3}-"
-#     r"[0-9a-fA-F]{12}\b"
-# )
+UUID4_REGEX = re.compile(
+    r"\b[0-9a-fA-F]{8}-"
+    r"[0-9a-fA-F]{4}-"
+    r"4[0-9a-fA-F]{3}-"
+    r"[89abAB][0-9a-fA-F]{3}-"
+    r"[0-9a-fA-F]{12}\b"
+)
 
-# def filter_uuid(logs):
-#     uuid_map = defaultdict(list)
-#     for log_line in logs:
-#         uuids = UUID4_REGEX.findall(log_line)
-#         for uuid in uuids:
-#             uuid_map[uuid].append(log_line)
-#     return uuid_map
+def filter_uuid(logs):
+    uuid_map = defaultdict(list)
+    for log_line in logs['logs']:
+        uuids = UUID4_REGEX.findall(log_line['message'])
+        for uuid in uuids:
+            uuid_map[uuid].append(log_line)
+    return uuid_map
